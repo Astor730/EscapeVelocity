1319278f6d752ab5440b24f651deb9bef1635704
Astor730 <42044772+Astor730@users.noreply.github.com>
2021-04-19 13:35:09 -0400

    Shooting Functionality

---
+++ Assets/Shooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public GameObject projectilePrefab;
+
+    public float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectile = Instantiate(projectilePrefab,
+                    transform.position + transform.forward, transform.rotation) as GameObject;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+
+            projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
+        }
+    }
+}
